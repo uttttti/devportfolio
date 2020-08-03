20330e1a4f7db59e5e1d875bb2479e3e41997bff
miyokohirao <uttttti@gmail.com>
2020-08-03 19:42:08 +0900

    ささやかやな修正

--- v2/src/components/About/About.tsx
+++ v2/src/components/About/About.tsx
@@ -3,13 +3,17 @@ import { Typography, Container } from '@material-ui/core';
 import Title from '../Title/Title';
 import './About.scss';
 
-const About: React.FC = () => (
-  <div className="About" data-testid="About" id="About">
-    <Container>
-      <Title label="About"></Title>
-      <Typography variant="body2">平尾 美代子です。Webディレクターやチームリーダー経験の方が長いですが、現在はフロントエンドエンジニア歴約4年です。ほぼ独学、かつトライ&エラーで乗り切ってきたので、知識も経験も最前線にいるわけではない自覚はありますが、どんな環境であっても意欲的に取り組む所存です。</Typography>
-    </Container>
-  </div>
-);
+const About: React.FC = () => {
+  const today = new Date();
+  const myYears = today.getFullYear() - 2016;
+  return (
+    <div className="About" data-testid="About" id="About">
+      <Container>
+        <Title label="About"></Title>
+        <Typography variant="body2">平尾 美代子です。Webディレクターやチームリーダー経験の方が長いですが、現在はフロントエンドエンジニア歴約{myYears}年です。ほぼ独学、かつトライ&エラーで乗り切ってきたので、知識も経験も最前線にいるわけではない自覚はありますが、どんな環境であっても意欲的に取り組む所存です。</Typography>
+      </Container>
+    </div>
+  );
+}
 
 export default About;
--- v2/src/components/Contact/Contact.tsx
+++ v2/src/components/Contact/Contact.tsx
@@ -8,8 +8,8 @@ const Contact: React.FC = () => (
   <div className="Contact" data-testid="Contact" id="Contact">
     <Typography variant="h4" color="secondary" className="title">Get in Touch</Typography>
     <IconButton href="https://github.com/uttttti"><FontAwesomeIcon icon={faGithub} color="#F3E5F5" size="lg"/></IconButton>
-    <IconButton href="https://www.facebook.com/uttttti"><FontAwesomeIcon icon={faTwitter} color="#F3E5F5" size="lg"/></IconButton>
-    <IconButton href="https://twitter.com/0345"><FontAwesomeIcon icon={faFacebook} color="#F3E5F5" size="lg"/></IconButton>
+    <IconButton href="https://www.facebook.com/uttttti"><FontAwesomeIcon icon={faFacebook} color="#F3E5F5" size="lg"/></IconButton>
+    <IconButton href="https://twitter.com/0345"><FontAwesomeIcon icon={faTwitter} color="#F3E5F5" size="lg"/></IconButton>
     <IconButton href="https://www.instagram.com/miyoko/"><FontAwesomeIcon icon={faInstagram} color="#F3E5F5" size="lg"/></IconButton>
   </div>
 );
